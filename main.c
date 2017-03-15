dfe67b7ae0fb4e05b40329c0c428c21df8570755
Jerem <jeremy.sernit@telecomnancy.net>
2017-03-15 09:17:10 +0100

    suppr

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include "const.h"
 #include "main.h"
+#include "SDL2/SDL.h"
 
 #include "tests.h"
 
@@ -30,9 +31,31 @@ void boardInit() {
     scanf("%hd", &player); // %hd used for a short int
 }
 
-int main(void) {
+int main(int argc, char** argv) {
     boardInit();
+
     // =========== TESTS ==========
     //testBoardInit(board);
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    {
+        fprintf(stdout, "Échec de l'initialisation de la SDL (%s)\n", SDL_GetError());
+        return -1;
+    }
+
+    {
+        SDL_Window* pWindow = NULL;
+        pWindow = SDL_CreateWindow("Awale", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+
+        if(pWindow) {
+            SDL_Delay(3000);
+            SDL_DestroyWindow(pWindow);
+        } else {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        }
+    }
+
+    SDL_Quit();
+
     return 0;
 }
