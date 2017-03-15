ec254c496050357aaf77f57b34a81fabe921735c
Olivier Bénard <olivier.benard@telecomnancy.eu>
2017-03-15 17:57:17 +0100

    ajout du .gitignore

---
+++ src/const.h
@@ -0,0 +1,15 @@
+/*
+ * const.h
+ *
+ *  Created on: 8 mars 2017
+ *      Author: Jerem
+ */
+
+#ifndef SRC_CONST_H_
+#define SRC_CONST_H_
+
+#define NB_ROW 2
+#define NB_HOLES 6
+#define NB_SEED_INIT 4
+
+#endif // SRC_CONST_H_
---
+++ src/movements.h
@@ -0,0 +1,22 @@
+/*
+ * movements.h
+ *
+ *  Created on: 8 mars 2017
+ *      Author: Jerem
+ */
+
+#ifndef SRC_MOVEMENTS_H_
+#define SRC_MOVEMENTS_H_
+
+/**
+ * Supprime les graines du trou selectionné et retourne le même nombre de graines
+ */
+short removeSeeds(short[][], short, short);
+
+/**
+ * Distribution des graines prises dans un trou dans les trous successifs
+ * de facon inverse des aiguilles d'une montre
+ */
+void distributeSeeds(short[][], short, short);
+
+#endif
