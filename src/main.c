fa4f783d35c9aa947cef72ff9bf77ee48ac1502a
Jerem <jeremy.sernit@telecomnancy.eu>
2017-03-16 20:28:09 +0100

    SDL init

--- src/main.c
+++ src/main.c
@@ -5,13 +5,14 @@
  *      Author: Jerem
  */
 
- #include <stdio.h>
- #include <stdlib.h>
- #include "const.h"
- #include "main.h"
- #include "write.h"
- #include "read.h"
- #include "tests.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDL2/SDL.h"
+#include "const.h"
+#include "main.h"
+#include "write.h"
+#include "read.h"
+#include "tests.h"
 
 // short au lieu de int car faibles unitees
 short board[NB_ROW][NB_HOLES];
@@ -31,8 +32,26 @@ void boardInit() {
     scanf("%hd", &player); // % hd used for a short int
 }
 
-int main(void) {
+int main(int argc, char** argv) {
     //boardInit();
+
+    SDL_Surface *screen;
+    if( SDL_Init(SDL_INIT_VIDEO) == -1) {
+        printf("Can't init SDL:  %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    atexit(SDL_Quit);
+    screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
+
+    if(screen == NULL) {
+        printf( "Can't set video mode: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Delay(3000);
+
+    return EXIT_SUCCESS;
 
     // =========== TESTS ==========
 
@@ -50,6 +69,4 @@ int main(void) {
     //read_specific(chemin, 3);
     //testBoardInit(board);
     */
-
-    return 0;
 }
