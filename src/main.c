83c9fd9abf4b5cb4c77e298ed98a82d72cea0c0b
Jerem <jeremy.sernit@telecomnancy.eu>
2017-03-22 00:13:59 +0100

    Première image de fond en SDL

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 #include <unistd.h> // utilser la fonction _sleep(temps_ms);
 #include <time.h>
 #include <SDL2/SDL.h> // il faut supprimer cet include pour afficher des printf sur la sortie standard
+#include <SDL2/SDL_image.h>
 #include <string.h>
 #include "const.h"
 #include "write.h"
@@ -20,57 +21,64 @@
 #include "ask.h"
 #include "main.h"
 
-int main(int argc, char** argv) {
+int SDLError(char* message) {
+    printf(message, SDL_GetError());
+    SDL_Quit();
+    return EXIT_FAILURE;
+}
 
-    // au demarrage proposer une interface sdl ou console (pb include SDL empeche le lancement console,
+int main(int argc, char* args[]) {
+    //Proposer une interface sdl ou console (pb include SDL empeche le lancement console,
     //il faudra gerer avec les variables pre processeur)
 
-    /*
+	short quit = 0;
+	SDL_Event event;
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
-	SDL_Texture* bitmapTex = NULL;
-	SDL_Surface* bitmapSurface = NULL;
+	SDL_Surface* surface = NULL;
+	SDL_Texture* texture = NULL;
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Can't init SDL : %s\n", SDL_GetError());
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) return SDLError("Can't init SDL : %s\n");
+	if(IMG_Init(IMG_INIT_JPG) < 0) return SDLError("Can't init SDL_image : %s\n");
 
-    atexit(SDL_Quit);
-    window = SDL_CreateWindow("Awale", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
-    							SDL_WINDOW_SHOWN | SDL_WINDOW_MOUSE_CAPTURE | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_MOUSE_FOCUS);
-
-    if(window == NULL) {
-        printf("Can't create window : %s\n", SDL_GetError());
-        SDL_Quit();
-        return EXIT_FAILURE;
-    }
+	window = SDL_CreateWindow("Awale", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
+								SDL_WINDOW_SHOWN | SDL_WINDOW_MOUSE_CAPTURE | SDL_WINDOW_MOUSE_FOCUS);
+	if(window == NULL) return SDLError("Can't create window : %s\n");
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(renderer == NULL) return SDLError("Can't create renderer : %s\n");
 
-    if(renderer == NULL) {
-        printf("Can't create renderer : %s\n", SDL_GetError());
-        SDL_Quit();
-        return EXIT_FAILURE;
+    char filename[PATH_SIZE] = "sprites/background.jpg";
+    surface = IMG_Load(filename);
+    if(surface == NULL) return SDLError("Can't create surface : %s\n");
+
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if(texture == NULL) return SDLError("Can't create texture : %s\n");
+
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+
+        switch (event.type) {
+            case SDL_QUIT: quit = 1;
+                break;
+        }
+
+        SDL_Rect rect = {SCREEN_WIDTH / 2 - surface -> w / 2, SCREEN_HEIGHT / 2 - surface -> h / 2, surface -> w, surface -> h};
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+        SDL_RenderPresent(renderer);
     }
 
-    bitmapSurface = SDL_LoadBMP("sprites/background.jpg");
-    bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
-    SDL_FreeSurface(bitmapSurface);
-
-    SDL_Delay(5000);
-
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    IMG_Quit();
     SDL_Quit();
+	return EXIT_SUCCESS;
+    //*/
 
-    return EXIT_SUCCESS;
-    */
-
-
-
-    // =========== TESTS ==========
+    /*/ =========== TESTS ==========
 
     //testSave();
     //testSaveInList();
@@ -82,5 +90,5 @@ int main(int argc, char** argv) {
 
     //testSave();
 
-    return 0;
+    return 0;//*/
 }
