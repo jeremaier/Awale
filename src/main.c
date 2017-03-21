7d7793ad7ab30cd4016c7d93c3ac8dcf8a69aa72
Jerem <jeremy.sernit@telecomnancy.eu>
2017-03-21 16:29:39 +0100

    corrections movements + SDL v1

--- src/main.c
+++ src/main.c
@@ -9,17 +9,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // utilser la fonction _sleep(temps_ms);
+#include <time.h>
+#include <SDL2/SDL.h> // il faut supprimer cet include pour afficher des printf sur la sortie standard
+#include <string.h>
 #include "const.h"
 #include "write.h"
 #include "read.h"
 #include "tests.h"
-//#include <SDL/SDL.h> // il faut supprimer cet include pour afficher des printf sur la sortie standard
 #include "game.h"
-#include "main.h"
-
 #include "ask.h"
-#include <string.h>
-#include <time.h>
+#include "main.h"
 
 int main(int argc, char** argv) {
 
@@ -27,22 +26,44 @@ int main(int argc, char** argv) {
     //il faudra gerer avec les variables pre processeur)
 
     /*
-    SDL_Surface *screen;
-    if( SDL_Init(SDL_INIT_VIDEO) == -1) {
-        printf("Can't init SDL:  %s\n", SDL_GetError());
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	SDL_Texture* bitmapTex = NULL;
+	SDL_Surface* bitmapSurface = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Can't init SDL : %s\n", SDL_GetError());
+        SDL_Quit();
         return EXIT_FAILURE;
     }
 
     atexit(SDL_Quit);
-    screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
+    window = SDL_CreateWindow("Awale", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
+    							SDL_WINDOW_SHOWN | SDL_WINDOW_MOUSE_CAPTURE | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_MOUSE_FOCUS);
 
-    if(screen == NULL) {
-        printf( "Can't set video mode: %s\n", SDL_GetError());
+    if(window == NULL) {
+        printf("Can't create window : %s\n", SDL_GetError());
+        SDL_Quit();
         return EXIT_FAILURE;
     }
 
-    SDL_Delay(3000);
-    FreeSurface ?
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if(renderer == NULL) {
+        printf("Can't create renderer : %s\n", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    bitmapSurface = SDL_LoadBMP("sprites/background.jpg");
+    bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
+    SDL_FreeSurface(bitmapSurface);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return EXIT_SUCCESS;
     */
@@ -55,7 +76,7 @@ int main(int argc, char** argv) {
     //testSaveInList();
 
     //playNewGame_c();
-       //playSavedGame_c();
+    //playSavedGame_c();
 
     play_ConsoleMode();
 
